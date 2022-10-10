be3036cad5f0a3e52eb75f8df8692870557d71bb
Công Thiện <91828383+congthienn@users.noreply.github.com>
2022-10-10 21:03:21 +0700

    Update SeedData

--- InventoryApp.Domain/Helper/SeedData/SeedDataSystemAdmin.cs
+++ InventoryApp.Domain/Helper/SeedData/SeedDataSystemAdmin.cs
@@ -1,5 +1,6 @@
 ﻿using InventoryApp.Data;
 using InventoryApp.Data.Models;
+using InventoryApp.Infrastructures.Helper;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,9 +14,14 @@ namespace InventoryApp.Domain.Helper.SeedData
         public static async void Run(IServiceProvider serviceProvider)
         {
             var dbContext = serviceProvider.GetService<InventoryDBContext>();
+            await SeedDataUser(serviceProvider, dbContext);
+                await SeedDataRole(serviceProvider, dbContext);
+        }
+        private static async Task SeedDataUser(IServiceProvider serviceProvider, DbContext dbContext)
+        {
             try
             {
-                if (!TableUserIsEmpty(dbContext).Result)
+                if (!dbContext.Set<Users>().AnyAsync().Result)
                 {
                     var userManagement = serviceProvider.GetService<UserManager<Users>>();
                     Guid guid = Guid.NewGuid();
@@ -37,9 +43,38 @@ namespace InventoryApp.Domain.Helper.SeedData
                 _log.Error(ex.Message);
             }
         }
-        private static async Task<bool> TableUserIsEmpty(DbContext dbContext)
+        private static async Task SeedDataRole(IServiceProvider serviceProvider, DbContext dbContext)
         {
-            return await dbContext.Set<Users>().AnyAsync();
+            List<string> listRole = new List<string>()
+            {
+                    ROLE_CONSTANT.SYSADMIN,
+                    ROLE_CONSTANT.ADMIN,
+                    ROLE_CONSTANT.DIRECTOR,
+                    ROLE_CONSTANT.MANAGER,
+                    ROLE_CONSTANT.EMPLOYEE,
+                    ROLE_CONSTANT.SUPPLIER,
+                    ROLE_CONSTANT.CUSTOMER,
+            };
+            try
+            {
+                if (!dbContext.Set<Roles>().AnyAsync().Result)
+                {
+                    var userManagement = serviceProvider.GetService<UserManager<Users>>();
+                    var roleManagement = serviceProvider.GetService<RoleManager<Roles>>();
+                    Users users = userManagement.FindByNameAsync("Systems").Result;
+                    foreach (var role in listRole)
+                    {
+                        Roles newRole = new Roles { Name = role };
+                        newRole.CreateBy(users);
+                        newRole.UpdateBy(users);
+                        await roleManagement.CreateAsync(newRole);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _log.Error(ex.Message);
+            }
         }
     }
 }
